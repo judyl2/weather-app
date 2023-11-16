280357bedc6d568c3c50b0c23d0cbff55808fb5d
judyl2 <149786355+judyl2@users.noreply.github.com>
2023-11-16 23:17:17 +0100

    Added search function using API

--- src/script.js
+++ src/script.js
@@ -1,11 +1,26 @@
+function refreshWeather(response) {
+	let temperatureElement = document.querySelector("#temperature");
+	let temperature = response.data.main.temp;
+	let cityElement = document.querySelector("#city-header");
+	cityElement.innerHTML = response.data.name;
+	temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+	let apiKey = "6bfa54f242cbb59343d4e58db578dc61";
+
+	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+	axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
 	event.preventDefault();
 	let searchInput = document.querySelector("#search-form-input");
-	let cityElement = document.querySelector("#city-header");
-	cityElement.innerHTML = searchInput.value;
+
+	searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-//console.log(searchInput.value);
+searchCity("Paris");
